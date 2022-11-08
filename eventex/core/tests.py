017e8d486259a6ee65befdd8989bf6c9b5dc0bff
Andre Moreira Leopoldino <andrem01310@gmail.com>
2022-11-07 21:07:16 -0300

    Habilita inscricao

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -16,4 +16,6 @@ class HomeTest(TestCase):
         self.assertTemplateUsed(self.response,'index.html')    
 
 
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
 
