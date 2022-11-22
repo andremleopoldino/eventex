12d324fe38656c13235b524cda21a73055e8e52c
Andre Moreira Leopoldino <andrem01310@gmail.com>
2022-11-22 11:08:39 -0300

    Refatorando testes e relações de template

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,50 @@
+
+from django.core import mail
+from django.test import TestCase
+
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='André Leopoldino', cpf='12345678901', email='andrem01310@gmail.com', phone = '11-98353-9575')
+        self.response = self.client.post('/inscricao/', data)
+        self.mail = mail.outbox[0]
+
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1,len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.mail.subject)
+
+    def test_subscription_email_from(self):
+        
+        expect = 'andrem01310@gmail.com'
+
+        self.assertEqual(expect, self.mail.from_email)
+
+    def test_subscription_email_to(self):
+        
+        expect = ['andrem01310@gmail.com' , 'andrem01310@gmail.com']
+
+        self.assertEqual(expect, self.mail.to)
+
+    
+    def test_subscription_message(self):
+        email = mail.outbox[0]
+        expect = 'Inscrição realizada com sucesso ! Obrigado pelo interesse no Eventex! O maior encontro de hackers do mundo!'
+
+        contents = [
+            'André Leopoldino',
+            '12345678901', 
+            'andrem01310@gmail.com',
+            '11-98353-9575'
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.mail.body)
+      
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -18,11 +18,20 @@ class SubscribeTest(TestCase):
 
     def test_html(self):
         """HTML must contain imput tags"""
-        self.assertContains(self.response, '<form')
-        self.assertContains(self.response, '<input', 6)
-        self.assertContains(self.response, 'type="text"',3)
-        self.assertContains(self.response, 'type="email"')
-        self.assertContains(self.response, 'type="submit"')
+        tags = (
+
+            ('<form', 1),
+            ('<input', 6),
+            ('type="text"', 3),
+            ('type="email"', 1),
+            ('type="submit"', 1)
+
+            )
+            
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
+       
 
     def test_csrf(self):
         """html must contain csrf"""
@@ -48,39 +57,6 @@ class SubscribePostTest(TestCase):
     def test_post(self):
 
         self.assertEqual(302, self.response.status_code)
-
-
-    def test_send_subscribe_email(self):
-        self.assertEqual(1,len(mail.outbox))
-
-    def test_subscription_email_subject(self):
-        email = mail.outbox[0]
-        expect = 'Confirmação de inscrição'
-
-        self.assertEqual(expect, email.subject)
-
-    def test_subscription_email_from(self):
-        email = mail.outbox[0]
-        expect = 'contato@eventex.com.br'
-
-        self.assertEqual(expect, email.from_email)
-
-    def test_subscription_email_to(self):
-        email = mail.outbox[0]
-        expect = ['contato@eventex.com.br' , 'andrem01310@gmail.com']
-
-        self.assertEqual(expect, email.to)
-
-    
-    def test_subscription_message(self):
-        email = mail.outbox[0]
-        expect = 'Inscrição realizada com sucesso ! Obrigado pelo interesse no Eventex! O maior encontro de hackers do mundo!'
-
-        self.assertIn('André Leopoldino', email.body)
-        self.assertIn('12345678901', email.body)
-        self.assertIn('andrem01310@gmail.com', email.body)
-        self.assertIn('11-98353-9575', email.body)
-
 
 
 class SubscribeInvalidPost(TestCase):
